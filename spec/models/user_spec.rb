4316a733fcaf4b429c897fd276f4307b3b53e8dc
yuta-chi <152576167+yuta-chi@users.noreply.github.com>
2024-01-08 17:58:00 +0900

    ルボコップの実施　テストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,27 @@ RSpec.describe User, type: :model do
       it '全ての入力事項が存在すれば登録できる' do
         expect(@user).to be_valid
       end
+      it 'パスワードは6文字以上半角英数字であれば登録できる' do
+        @user.password = '123abc'
+        @user.password_confirmation = '123abc'
+        expect(@user).to be_valid
+      end
+      it '名字が全角（漢字・ひらがな・カタカナ）であれば登録できる' do
+        @user.last_name = '山田'
+        expect(@user).to be_valid
+      end
+      it '名前が全角（漢字・ひらがな・カタカナ）であれば登録できる' do
+        @user.first_name = '陸太郎'
+        expect(@user).to be_valid
+      end
+      it '名字のフリガナが全角（カタカナ）であれば登録できる' do
+        @user.last_name_kana = 'ヤマダ'
+        expect(@user).to be_valid
+      end
+      it '名前のフリガナが全角（カタカナ）であれば登録できる' do
+        @user.first_name_kana = 'リクタロウ'
+        expect(@user).to be_valid
+      end
     end
 
     context '新規登録ができない場合' do
