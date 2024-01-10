96507955bc3bbaaad1743e9b470a624c7ce0363f
yuta-chi <152576167+yuta-chi@users.noreply.github.com>
2024-01-10 12:31:36 +0900

    Rubocop実施

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   # ログインしていないユーザーはログインページに
-  before_action :authenticate_user!, except: [:index,]
+  before_action :authenticate_user!, except: [:index]
 
   def index
     # アクションの処理を実装する
@@ -22,6 +22,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:image, :name, :description, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:product).permit(:image, :name, :description, :category_id, :condition_id, :delivery_cost_id, :prefecture_id,
+                                    :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
