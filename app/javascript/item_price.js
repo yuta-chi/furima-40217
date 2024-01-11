b751908a5c13765af3285f7a624e5030415d4c59
yuta-chi <152576167+yuta-chi@users.noreply.github.com>
2024-01-11 12:54:57 +0900

    商品価格表示の変更
    トップページから出品ページに遷移した際、価格によって手数料等が表示されなかかったため

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,12 @@
-window.addEventListener('load', () => {
+document.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    const ProfitDom = document.getElementById("profit");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-    ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
-  })
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      const ProfitDom = document.getElementById("profit");
+      addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+      ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+    });
+  }
 });
