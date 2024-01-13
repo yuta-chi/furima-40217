9a3845febb47683d840db88e52220c8b763c9077
yuta-chi <152576167+yuta-chi@users.noreply.github.com>
2024-01-13 20:10:44 +0900

    レビュー修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -11,7 +11,7 @@ class OrderForm
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :municipalities
     validates :street_address
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
 
     validates :token
   end
